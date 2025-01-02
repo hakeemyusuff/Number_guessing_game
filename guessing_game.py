ae7d7091ee2141eba5895d4e69f44d7f8ee4c551
Yusuff Abdulhakeem <hakeemyusuff19@gmail.com>
2025-01-02 14:10:01 +0100

    Created the game main file

---
+++ guessing_game.py
@@ -0,0 +1,9 @@
+#! usr/bin/python3
+
+#TO DO
+#Display welcome message along with the rule of the game
+#define a function that randomly select a number between 1 to 100
+#define a function that handles difficulty
+#Handle input from user
+#Define a function that handles if the user guess the random number correctly
+
